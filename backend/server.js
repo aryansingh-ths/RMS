ffe5a2fca5ace6b0db197e0bc0b7e41d812dc735
TechhansaSupport <Support@techhansait.com>
2026-09-16 11:29:55 +0530

    resend api key moved to env file

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const sendConfirmationEmail = async (email, guestName, authCode) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer re_HSDjYy61_2GZdgEwXzD1vamAPu5XoEXAV`
+        'Authorization': `Bearer ${process.env.RESEND_API_KEY}`
       },
       body: JSON.stringify({
         from: 'Pragati RMS <onboarding@resend.dev>',
